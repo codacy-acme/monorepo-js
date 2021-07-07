a94ecc02b4c177ff4c260f3d62d6d0271daa7edd
Hélio Rocha <hjrocha@gmail.com>
2021-07-07 13:04:39 +0100

    add client and server

---
+++ client/api.js
@@ -0,0 +1,13 @@
+function foo1(a, b, a) {
+    typeof a === undefined
+    console.log("value of the second a:", a);
+}
+
+var bar = function (a, b, a) {
+    console.log("value of the second a:", a);
+};
+
+var damn = function (a, b) {
+    console.log("value of the second a:", a);
+    return a*b;
+};
